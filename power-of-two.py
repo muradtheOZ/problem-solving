043f526a795dfbda3d5f5b61cf3298e7d90aa62b
Murad Ahmed <muradahmed199771@gmail.com>
2021-11-18 22:52:02 +0600

    power of two

---
+++ power-of-two.py
@@ -0,0 +1,15 @@
+def isPowerOfTwo(n):
+    i = 5
+    if n<= 16 and (n == 1 or n == 2 or n== 4 or n==8 or n== 16):
+        return True
+    elif n > 16:
+        while i <= n//4:
+            if pow(2,i) == n:
+                return True
+            elif pow(2,i) > n:
+                return False
+            i += 1
+        else:
+            return False
+result = isPowerOfTwo(64)
+print(result)
