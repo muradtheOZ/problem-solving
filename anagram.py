c090bbd72828a089c3ab026366ce8f01a228cef9
Murad Ahmed <muradahmed199771@gmail.com>
2021-11-19 14:18:20 +0600

    anagram problem 25 second version

--- anagram.py
+++ anagram.py
@@ -1,10 +1,8 @@
 def isAnagram(s, t):
 
     def sorted_string(value):
-        making_tuple = ",".join(value)
-        sorting_tuple = sorted(making_tuple)
-        remove_otherthan_englishletter = ("".join([ c if c >= "a" and c<= "z" else "" for c in sorting_tuple ]))
-        return remove_otherthan_englishletter
+        sorting_tuple = sorted(value)
+        return sorting_tuple
 
     sorted_string_one = sorted_string(s)
     sorted_string_two = sorted_string(t)
@@ -14,6 +12,6 @@ def isAnagram(s, t):
     else:
         return False
 
-a = "zsdikjlacxck"
-b = "xckczsajldik"
+a = "zsdikjlacxcklk"
+b = "xckczsajldiklk"
 print(isAnagram(a,b))
