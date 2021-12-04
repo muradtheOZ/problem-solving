9a5f93e8622e1f3e48847c208750a99249e2ed1b
Murad Ahmed <muradahmed199771@gmail.com>
2021-12-04 17:37:17 +0600

    problem nine dynamic solved

--- nine-grid-unique-paths.py
+++ nine-grid-unique-paths.py
@@ -1,9 +1,20 @@
 from math import factorial
 
 def grid_unique_paths(A,B):
-    A -= 1
-    B -= 1
-    return factorial(A+B) // (factorial(A) * factorial(B))
+    grid = []
+
+    for _ in range(A):
+        grid.append([0]*B)
+    for j in range(B):
+        grid[A-1][j] = 1
+
+    for i in range(A):
+        grid[i][B-1] = 1
+    for i in range(A-2,-1,-1):
+        for j in range(B-2, -1, -1):
+            grid[i][j] = grid[i][j+1] + grid[i+1][j]
+
+    return grid[0][0]
 
 
-print(grid_unique_paths(3,3))
+print(grid_unique_paths(7,3))
