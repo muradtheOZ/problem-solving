6b393828535b90edda5eee8ccf2dc084ae6d21fc
Murad Ahmed <muradahmed199771@gmail.com>
2021-12-05 17:00:32 +0600

    reafactor the parameter to match with leetcode def

--- K-frequent/top-k-freqient-solve-one.py
+++ K-frequent/top-k-freqient-solve-one.py
@@ -1,17 +1,17 @@
-def frequent_k_element(list,k):
-    list.sort()
+def frequent_k_element(nums,k):
+    nums.sort()
     new_list = []
     result = []
     count = 1
-    for i in range (len(list)):
-        if i < len(list)-1 and list[i] == list[i +1]:
+    for i in range (len(nums)):
+        if i < len(nums)-1 and nums[i] == nums[i +1]:
             count += 1
             continue
-        new_list.append((count,list[i]))
+        new_list.append((count,nums[i]))
         count = 1
     new_list.sort(reverse=True)
     #print (new_list)
-    for i in range(k +1):
+    for i in range(k):
         result.append(new_list[i][1])
     return result
 
