27618dbcb8e6862726653fe799e122652b35f9cf
Murad Ahmed <muradahmed199771@gmail.com>
2021-12-07 23:24:36 +0600

    binary add another solution done

---
+++ ninteen-binary-add-2.py
@@ -0,0 +1,25 @@
+def add_binary(a,b):
+    a = a[::-1]
+    b = b[::-1]
+
+    len_a,len_b = len(a),len(b)
+    if len_a < len_b:
+        a = a + "0"*(len_b-len_a)
+    elif len_b < len_a:
+        b = b + "0"*(len_a - len_b)
+    output = []
+    carry = 0
+     
+    for i in range (len(a)):
+        result = (ord(a[i])-48) + (ord(b[i])-48) + carry
+        carry = result //2
+        result = result % 2
+        output.append(str(result))
+
+    if carry == 1:
+        output.append(str(carry))
+    return "".join(output[::-1])
+
+print(add_binary("1010","1011"))
+    
+        
